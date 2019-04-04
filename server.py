1318e392dedf61238aae5266ccb47b89562a3f90
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-04-04 11:45:53 +0530

    basic login

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 from bottle import Bottle, run, template, static_file, request, route, redirect, error
 import os
-from collections import namedtuple
+from collections import namedtuple, defaultdict
 
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
 app = Bottle()
 
-users = namedtuple("user","password")
-moderator = namedtuple("moderator","password")
+User = namedtuple("User", "password")
+Moderator = namedtuple("Moderator","password")
+users = defaultdict()
+moderators = defaultdict()
+ambulances = defaultdict()
+# dummy accounts
+moderators["rishabhkalakoti@gmail.com"]=Moderator(password = "letmepass")
+users["8440949302"]=User(password = "letmepass")
 
 @app.route("/")
 def changePath():
@@ -18,8 +24,34 @@ def error404(error):
     return redirect("/")
 
 @app.get("/login")
-def download():
+def login():
     return template("templates/login.html")
+
+@app.post("/verifyMod")
+def verifyMod():
+	user_id = request.forms.get("email")
+	pwd = request.forms.get("pwd")
+	if(user_id in moderators):
+		if(moderators[user_id].password == pwd):
+			return "Logged in"
+	return "Incorrect username/password"
+
+@app.post("/verifyUser")	
+def verifyUser():
+	user_id = request.forms.get("user_id")
+	pwd = request.forms.get("pwd")
+	if(user_id in moderators):
+		if(moderators[user_id].password == pwd):
+			return "Logged in"
+	return "Incorrect username/password"
+
+@app.get("/dashboard")
+def dashboard():
+	return "dash"
+
+@app.get("/booking")
+def booking():
+	return "book"
 
 @app.get("/static/<filepath:path>")
 def server_static(filepath):
