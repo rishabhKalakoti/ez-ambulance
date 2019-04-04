96726f77d871c13440573f89084c10f83a3649f3
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-04-04 13:09:42 +0530

    added sessions

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 import os
 from collections import namedtuple, defaultdict
 
@@ -30,6 +30,8 @@ def verifyMod():
 	if(user_id in moderators):
 		if(moderators[user_id].password == pwd):
 			#todo: create session
+			session["logged_in"] = "mod"
+			session["user_id"] = user_id
 			return redirect("/dashboard")
 	return "Incorrect username/password"
 
@@ -40,20 +42,27 @@ def verifyUser():
 	if(user_id in moderators):
 		if(moderators[user_id].password == pwd):
 			# todo: create session
+			session["logged_in"] = "user"
+			session["user_id"] = user_id
 			return redirect("/booking")
 	return "Incorrect username/password"
 
 @app.route("/dashboard")
 def dashboard():
+	if not (session.get("logged_in")=="mod"):
+		return render_template("login.html")
 	return render_template("dashboard.html")
 
 @app.route("/booking")
 def booking():
+	if not (session.get("logged_in")=="user"):
+		return render_template("login.html")
 	return render_template("booking.html")
 
 @app.route("/logout")
 def logout():
-	# todo- destroy session
+	session["logged_in"]=False
+	session["user_id"]=None
 	return render_template("login.html")
 
 
@@ -61,4 +70,6 @@ def logout():
 def server_static(path):
     return app.send_static_file("static/" + path)
 
+
+app.secret_key = os.urandom(12)
 app.run(host="localhost", port=8080)
