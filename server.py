56546a4ee09700b6ee50939cdc9ef9ed10b8024d
Rishabh Kalakoti <rishabh.kalakoti@gmail.com>
2019-04-04 10:35:07 +0530

    simple server

--- server.py
+++ server.py
@@ -1,7 +1,25 @@
-from bottle import Bottle, run, template, static_file, request, route, redirect
+from bottle import Bottle, run, template, static_file, request, route, redirect, error
 import os
+from collections import namedtuple
+
 path = os.path.abspath(__file__)
 dir_path = os.path.dirname(path)
+app = Bottle()
+
+users = namedtuple("user","password")
+moderator = namedtuple("moderator","password")
+
+@app.route("/")
+def changePath():
+    return template("templates/login.html")
+
+@error(404)
+def error404(error):
+    return redirect("/")
+
+@app.get("/login")
+def download():
+    return template("templates/login.html")
 
 @app.get("/static/<filepath:path>")
 def server_static(filepath):
